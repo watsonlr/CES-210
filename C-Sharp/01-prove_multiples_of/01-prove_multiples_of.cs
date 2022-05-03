6a39bb9d34a2b3007f27d38936851519f4e63ee4
watsonlr <watsonl@byui.edu>
2022-05-03 06:27:26 -0600

    Good Enough

--- C-Sharp/01-prove_multiples_of/01-prove_multiples_of.cs
+++ C-Sharp/01-prove_multiples_of/01-prove_multiples_of.cs
@@ -33,20 +33,23 @@ static List<double> multiples_of(double number, int length) {
     }
 */
 
-multiples_of_class set1 = new multiples_of_class();
-set1.PrintArray(set1.multiples_of(7.0D,5));
-set1.PrintArray(set1.multiples_of(1.5D,10));
-set1.PrintArray(set1.multiples_of(-2,10));
-// set1.PrintArray(the_array);
+multiples_of_class mult_of_instance = new multiples_of_class();
+List<double> the_array1;
+
+the_array1 = mult_of_instance.multiples_of(7.0D,5);    // Just the array is returned in mult_of_instance.multiples
+mult_of_instance.PrintArray(the_array1);                // Shows the array can be broken out 
+// Or just make & show in one step.  Note that the method creates a new instance of the list each time it is called
+mult_of_instance.PrintArray(mult_of_instance.multiples_of(1.5D,10));
+mult_of_instance.PrintArray(mult_of_instance.multiples_of(-2,10));
+// mult_of_instance.PrintArray(the_array);
 
 
 public class multiples_of_class
     {
     // Storage for the list -- kept in the class, then returned
-    // Public method that creates the list when instantiated
-
     //public List<double> toreturn = new List<double>();
-    public int plength;
+    public int plength;  // make the length available to the PrintArray()
+    // Public method "multiples_of" creates the list (array) when called with the parameters of how many
     public List<double> multiples_of(double number, int length)
         {
         plength = length;
@@ -56,11 +59,14 @@ public class multiples_of_class
             { toreturn.Add(i * number); }
         return toreturn;
         }
+    // Public method "PrintArray" prints out the elements of the  (array) with commas to show separation
+
     public void PrintArray(List<double> toprint){
         int i = 1;
+        Console.Write("[");
         foreach (var item in toprint) {
             Console.Write(String.Format("  {0:F1}", item));i++;
-            if (i<=plength) {Console.Write(",");} else {Console.WriteLine();}
+            if (i<=plength) {Console.Write(",");} else {Console.WriteLine(" ]");}
             }
         }
     }
@@ -68,12 +74,8 @@ public class multiples_of_class
 
 
 
-
-//Console.WriteLine(multiples_of(7,5)[2]);    // [7, 14, 21, 28, 35]
-//Console.WriteLine(test1[1]);    // [7, 14, 21, 28, 35]
-//Console.WriteLine(multiples_of(7, 5));    // [7, 14, 21, 28, 35]
-// Console.ReadLine();
-/*
-Console.WriteLine(multiples_of(1.5, 10)); //  [1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0]
-Console.WriteLine(multiples_of(-2, 10));  //  [-2, -4, -6, -8, -10, -12, -14, -16, -18, -20]
+/* (number,length)      Answer
+    (7, 5))             [7, 14, 21, 28, 35]
+    (1.5, 10)           [1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0]
+    (-2, 10))           [-2, -4, -6, -8, -10, -12, -14, -16, -18, -20]
 */
