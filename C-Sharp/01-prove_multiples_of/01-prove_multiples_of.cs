51de48f295660d7b5f6bb38e97b0af45bc3c3807
watsonlr <watsonl@byui.edu>
2022-05-03 05:52:46 -0600

    Seems to work

--- C-Sharp/01-prove_multiples_of/01-prove_multiples_of.cs
+++ C-Sharp/01-prove_multiples_of/01-prove_multiples_of.cs
@@ -18,27 +18,62 @@ Console.WriteLine("CSE212:  01-Prove - Problem 1");  // Comment out this line
         //     integer greater than 0.  The implementation must be
         //     done using a list comprehension.
         //     
-float[] multiples_of(double number, int length) {
-    float[] toreturn = new float[length];
-        foreach (var i in Enumerable.Range(1, length )) {
-            toreturn.append(i * number);
-        return toreturn;
+List<double> test1 = new List<double>();
+test1.Add(1.0D);
+test1.Add(2.2D);
+test1.Add(3.3D);
 
-
-
-    }
-static object hold(double number, int length) {
-        var toreturn = new List<object>();
-        foreach (var i in Enumerable.Range(1, length )) {
-            toreturn.append(i * number);
+/*
+static List<double> multiples_of(double number, int length) {
+    List<double> toreturn = new List<double>();
+    foreach (var i in Enumerable.Range(1, length )) {
+        toreturn.Add(i * number);
         }
-        return toreturn;
+    return toreturn;
     }
+*/
 
-Console.WriteLine(multiples_of(7, 5));    // [7, 14, 21, 28, 35]
-Console.ReadLine();
+multiples_of_class set1 = new multiples_of_class();
+set1.PrintArray(set1.multiples_of(7.0D,5));
+set1.PrintArray(set1.multiples_of(1.5D,10));
+set1.PrintArray(set1.multiples_of(-2,10));
+// set1.PrintArray(the_array);
+
+
+public class multiples_of_class
+    {
+    // Storage for the list -- kept in the class, then returned
+    // Public method that creates the list when instantiated
+
+    //public List<double> toreturn = new List<double>();
+    public int plength;
+    public List<double> multiples_of(double number, int length)
+        {
+        plength = length;
+        // public int i = length;
+        List<double> toreturn = new List<double>();
+        foreach (var i in Enumerable.Range(1, length ))
+            { toreturn.Add(i * number); }
+        return toreturn;
+        }
+    public void PrintArray(List<double> toprint){
+        int i = 1;
+        foreach (var item in toprint) {
+            Console.Write(String.Format("  {0:F1}", item));i++;
+            if (i<=plength) {Console.Write(",");} else {Console.WriteLine();}
+            }
+        }
+    }
+// example1 = new makeMultiplesClass.multiples_of(7,5);
+
+
+
+
+//Console.WriteLine(multiples_of(7,5)[2]);    // [7, 14, 21, 28, 35]
+//Console.WriteLine(test1[1]);    // [7, 14, 21, 28, 35]
+//Console.WriteLine(multiples_of(7, 5));    // [7, 14, 21, 28, 35]
+// Console.ReadLine();
 /*
 Console.WriteLine(multiples_of(1.5, 10)); //  [1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0]
 Console.WriteLine(multiples_of(-2, 10));  //  [-2, -4, -6, -8, -10, -12, -14, -16, -18, -20]
 */
-}
