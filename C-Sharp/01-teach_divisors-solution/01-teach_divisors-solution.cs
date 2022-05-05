313272dbec61ffa7fdab495eee56a624fcc29ec4
watsonlr <watsonl@byui.edu>
2022-05-05 07:10:43 -0600

    Finished -- Uses Lists because of indeterminate length of answer array

--- C-Sharp/01-teach_divisors-solution/01-teach_divisors-solution.cs
+++ C-Sharp/01-teach_divisors-solution/01-teach_divisors-solution.cs
@@ -12,51 +12,48 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 Console.WriteLine("CSE212:  01-Teach - Problem 1 - Solution");  // Comment out this line
 
 /* 
-def find_divisors_1(number):
+def find_divisors(number):
     /*
     Create a list of all divisors for a number including 1
     and excluding the number itself.  Modulo will be used
     to test divisibility.
     Test all numbers from 1 to number-1.
 
-    Actually, we can be more efficient with range(1, (number//2)+1)
+    Actually, we can be could efficient with range(1, (number//2)+1)
+    Not shown here.
     */
 
-/*   TO REPLACE
-def find_divisors_2(number):
-    """
-    Same as find_divisors_1 but a list comprehension is used.
-    """
-    divisors = [n for n in range(1, number) if number % n == 0]
-    return divisors
-*/
+/* Note that the List type is used because we don't know in advance, the number of 
+   Divisors to be in the array  */
 
-// int[] find_divisors_1(int number) {
-List<int> find_divisors_1(int number) {
+// ******************************** //
+List<int> find_divisors(int number) {
+// ******************************** //
     List<int> find_them = new List<int>();
-    //make instance of the list_type
     int found = 0;
-
-    for(int divisor=1;(divisor<=number);divisor++) {
-        if (number % divisor == 0) {find_them.Append(number);}
-        found++;
-        }
-    Console.WriteLine("Found: " + found);
-    return find_them;
-    }
-
-/* 
-int[] find_divisors_2(int number) {
-    int[] find_them = new List<int>();
-    for(int divisor=1;(divisor<=number);divisor++) {
-        if (number % divisor == 0) {find_them.Append(number);}
+    for(int divisor=1;(divisor<number);divisor++) {
+        if ((number % divisor) == 0) {
+            find_them.Add(divisor); found++;
+            }
         }
     return find_them;
     }
-*/
 
-List<int> answer = find_divisors_1(80); // Answer should be:   [1, 2, 4, 5, 8, 10, 16, 20, 40]
-// Console.WriteLine(find_divisors_1(80)); // Answer should be:   [1, 2, 4, 5, 8, 10, 16, 20, 40]
-// Console.WriteLine(find_divisors_2(80)); // Answer should be:   [1, 2, 4, 5, 8, 10, 16, 20, 40]
-// Console.WriteLine(find_divisors_1(79)); // Answer should be:   [1] ... This is prime
-// Console.WriteLine(find_divisors_2(79)); // Answer should be:   [1]
+
+// ******************************** //
+ void PrintArray(List<int> toprint) {
+// ******************************** //
+        Console.Write("[");
+        for(int i=0;i<toprint.Count;i++)
+            {
+                if (i==(toprint.Count-1))
+                    {Console.WriteLine(toprint[i] + "]");}
+                else {Console.Write(toprint[i] + ", ");}
+            }
+        }
+
+
+// *********** EXECUTION START HERE ********************* //
+List<int> answer1 = find_divisors(80);                            // Answer should be: [1, 2, 4, 5, 8, 10, 16, 20, 40]
+Console.Write("Divisors of 80: "); PrintArray(answer1);
+Console.Write("Divisors of 79: "); PrintArray(find_divisors(79)); // Answer should be: [1]    This is a prime number
