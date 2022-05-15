7cf2937b2170f1474c72ff4381528fddc23f0e62
watsonlr <watsonl@byui.edu>
2022-05-15 08:30:02 -0600

    initial changes begun

--- C-Sharp/04-prove_taking_turns_queue/04-prove_taking_turns_queue-lrw.cs
+++ C-Sharp/04-prove_taking_turns_queue/04-prove_taking_turns_queue-lrw.cs
@@ -14,7 +14,7 @@ using System;
 
 Console.WriteLine("CSE212:  04-Prove - Problem 1");  // Comment out this line
 
-Queue theTurnsQueue = new Queue();
+Queue theTurnsQueue = new turnsQueue();
 
 //     
 public class Queue {
@@ -43,121 +43,111 @@ public class Queue {
     public bool is_empty() {
         return (theQueue.Count == 0);
         }
-    
     // 
     //         Support the len() function
     //         
     public int Count() {
         return theQueue.Count;
     }
+}
+
+public class turnsQueue(){
     // 
-    //         Provide a string representation for this object.
-    //         
+    //     This queue is circular.  When people are added via add_person, then they are added to the 
+    //     back of the queue (per FIFO rules).  When get_next_person is called, the next person
+    //     in the queue is displayed and then they are placed back into the back of the queue.  Thus,
+    //     each person stays in the queue and is given turns.  When a person is added to the queue, 
+    //     a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
+    //     less than they will stay in the queue forever.  If a person is out of turns then they will 
+    //     not be added back into the queue.
+    //     
+    // has specialized add/delete instruction that make this circular
+
+
+    }
+
+
+
+
+
+
+
+public class Person {
+//         A person is defined with a name and a number of turns.
+    public string name; 
+    public int turns; 
+
+    public Person(string name_in, int turns_in) {
+        name = name_in;
+        turns = turns_in;
+    }
+    public void showMe(){
+        Console.WriteLine("Name: "+ name + " Turns: " + turns);
+        }
+    // 
+    //             Support the display of single person.
+    //             
 }
 
 // 
-//     This queue is circular.  When people are added via add_person, then they are added to the 
-//     back of the queue (per FIFO rules).  When get_next_person is called, the next person
-//     in the queue is displayed and then they are placed back into the back of the queue.  Thus,
-//     each person stays in the queue and is given turns.  When a person is added to the queue, 
-//     a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-//     less than they will stay in the queue forever.  If a person is out of turns then they will 
-//     not be added back into the queue.
-//     
-public class Taking_Turns_Queue {
-    
-    // 
-    //         A person is defined with a name and a number of turns.
-    //         
-    public class Person {
-        
-        public Person(object name, object turns) {
-            this.name = name;
-            this.turns = turns;
+//         Add new people to the queue with a name and number of turns
+//         
+public virtual object add_person(object name, object turns) {
+    var person = Taking_Turns_Queue.Person(name, turns);
+    this.people.enqueue(person);
+}
+// 
+//         Get the next person in the queue and display them.  The person should
+//         go to the back of the queue again unless the turns variable shows that they 
+//         have no more turns left.  Note that a turns value of 0 or less means the 
+//         person has an infinite number of turns.  An error message is displayed 
+//         if the queue is empty.
+//         
+public virtual object get_next_person() {
+    if (this.people.is_empty()) {
+        Console.WriteLine("No one in the queue.");
+    } else {
+        var person = this.people.dequeue();
+        if (person.turns > 1) {
+            person.turns -= 1;
+            this.people.enqueue(person);
         }
-        
-        // 
-        //             Support the display of single person.
-        //             
-        public override object ToString() {
-            object result;
-            if (this.turns <= 0) {
-                result = "({}:Forever)".format(this.name);
-            } else {
-                result = "({}:{})".format(this.name, this.turns);
-            }
-            return result;
-        }
-    }
-    
-    public Taking_Turns_Queue() {
-        this.people = Queue();
-    }
-    
-    // 
-    //         Add new people to the queue with a name and number of turns
-    //         
-    public virtual object add_person(object name, object turns) {
-        var person = Taking_Turns_Queue.Person(name, turns);
-        this.people.enqueue(person);
-    }
-    
-    // 
-    //         Get the next person in the queue and display them.  The person should
-    //         go to the back of the queue again unless the turns variable shows that they 
-    //         have no more turns left.  Note that a turns value of 0 or less means the 
-    //         person has an infinite number of turns.  An error message is displayed 
-    //         if the queue is empty.
-    //         
-    public virtual object get_next_person() {
-        if (this.people.is_empty()) {
-            Console.WriteLine("No one in the queue.");
-        } else {
-            var person = this.people.dequeue();
-            if (person.turns > 1) {
-                person.turns -= 1;
-                this.people.enqueue(person);
-            }
-            Console.WriteLine(person.name);
-        }
-    }
-    
-    // 
-    //         Support the len() function
-    //         
-    public virtual object @__len__() {
-        return this.people.Count;
-    }
-    
-    // 
-    //         Provide a string representation of everyone in the queue
-    //         
-    public override object ToString() {
-        return this.people.ToString();
+        Console.WriteLine(person.name);
     }
 }
 
-public static object players = Taking_Turns_Queue();
+// 
+//         Support the len() function
+//         
+public virtual object @__len__() {
+    return this.people.Count;
+}
+
+// 
+//         Provide a string representation of everyone in the queue
+//         
+public override object ToString() {
+    return this.people.ToString();
+}
+
+public static object theTurnsQueue = Taking_Turns_Queue();
 
 testing() {
-    players.add_person("Bob", 2);
-    players.add_person("Tim", 5);
-    players.add_person("Sue", 3);
-    players.get_next_person();
-    players.add_person("Bob", 2);
-    players.add_person("Tim", 5);
-    players.add_person("Sue", 3);
-    players.get_next_person();
-    players.add_person("George", 3);
-    players.get_next_person();
-    players.add_person("Bob", 2);
-    players.add_person("Tim", 0);
-    players.add_person("Sue", 3);
-    players.get_next_person();
-    players.get_next_person();
+    theTurnsQueue.add_person("Bob", 2);
+    theTurnsQueue.add_person("Tim", 5);
+    theTurnsQueue.add_person("Sue", 3);
+    theTurnsQueue.get_next_person();
+    theTurnsQueue.add_person("Bob", 2);
+    theTurnsQueue.add_person("Tim", 5);
+    theTurnsQueue.add_person("Sue", 3);
+    theTurnsQueue.get_next_person();
+    theTurnsQueue.add_person("George", 3);
+    theTurnsQueue.get_next_person();
+    theTurnsQueue.add_person("Bob", 2);
+    theTurnsQueue.add_person("Tim", 0);
+    theTurnsQueue.add_person("Sue", 3);
+    theTurnsQueue.get_next_person();
+    theTurnsQueue.get_next_person();
 }
 
-public static object players = Taking_Turns_Queue();
-public static object players = Taking_Turns_Queue();
-public static object players = Taking_Turns_Queue();
 }
