9f6295fbb3c9bc958e8cdd54dee66b54e63282d0
watsonlr <watsonl@byui.edu>
2022-05-16 06:06:49 -0600

    Ready_to_compile

--- C-Sharp/04-teach_customer_service-solution/04-teach_customer_service-solution-lrw.cs
+++ C-Sharp/04-teach_customer_service-solution/04-teach_customer_service-solution-lrw.cs
@@ -11,21 +11,67 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 ********/
 Console.WriteLine("CSE212:  04-Teach - Problem 2 - Solution");  // Comment out this line
 
+customerService myServiceQueue = new customerService();
 
-public class customerService{
+/***   TESTING ***/
+Console.WriteLine("Test 2");
+myServiceQueue.add_new_customer();
+myServiceQueue.add_new_customer();
+/*
+Console.WriteLine("Before serving customers: ",service);
+service.serve_customer()
+service.serve_customer()
+Console.WriteLine("After serving customers: ",service);
+*/
+
+
+
+
+class customerService{
+    public customerServiceQueue customers_to_serve = new customerServiceQueue();
+    int customer_number = 1;  // should be unique and ever-increasing
+    
+    public void add_new_customer(){
+        Customer new_one = new Customer();
+        new_one.accountID = customer_number++;
+        customers_to_serve.EnQueue(new_one);
+        }
+
+    public class Customer {
+        public int accountID; 
+        public string name = "";
+        public string problem = "";
+        public Customer() {
+            Console.Write("Enter the Customer name --> ");
+            name = Console.ReadLine();
+            Console.Write("Enter the Problem --> ");
+            problem = Console.ReadLine();
+            }
+        }
     
     public class customerServiceQueue {
         public List<Customer> theQueue = new List<Customer>();
         // 
+
+        public void serve_customer(){
+            /* For now, this just prints the name of the customer and takes him off the queue */
+            if (empty()) {
+                Console.WriteLine("No more customers left to serve");
+            } else {
+                Customer to_serve = DeQueue();
+                Console.WriteLine("Customer[#" + "]: " + to_serve.name + "\tProblem: " + to_serve.problem);
+                }
+        }
+
         public void EnQueue(Customer to_add) {
             //         Add an item to the queue
             // Console.WriteLine("Node_to_add priority:  " + node_to_add.priority);
             // Console.WriteLine("Enqueueing:  " + node_to_add.ToString());
             theQueue.Add(to_add);
             }
-
         public Customer DeQueue() {
-            Customer empty = new Customer("Empty",-1);
+            Customer empty = new Customer();
+            empty.name="Empty";empty.accountID=0;
             if (theQueue.Count == 0) {
                 Console.WriteLine("Queue EMPTY");
                 return empty;
@@ -42,15 +88,15 @@ public class customerService{
             Console.WriteLine("\t\tQueue Count is:  " +theQueue.Count);
             for(int i=0;i<theQueue.Count; i++) {
                 Console.WriteLine("\t\t[" + i + "]\t" + theQueue[i].name + "\t turns:\t" + theQueue[i].turns);
+                }
             }
-        }
         public int Count() {
             return theQueue.Count;
             }
-    public class Customer {
-        public int accountID; 
-        public string name = "";
-        public string problem = ""; 
-        name = Console.ReadLine();
+        public bool empty() {
+            return (theQueue.Count==0);
         }
+
+    }
+
     }
