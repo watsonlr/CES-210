3655478fea1d593fbb50a391053683441a1568d9
Lynn Watson <watsonl@byui.edu>
2022-05-21 17:15:52 -0600

    Initial commit,  checking from MBP

--- C-Sharp/05-teach_display_sums-solution/05-teach_display_sums-solution-lrw.cs
+++ C-Sharp/05-teach_display_sums-solution/05-teach_display_sums-solution-lrw.cs
@@ -16,26 +16,28 @@ Console.WriteLine("CSE212:  05-Teach - Problem 2 - Solution");  // Comment out t
     Display pairs of numbers that sum to 10 using a set in O(n) time
     We are assuming that there are no duplicates in the list
     */
-    
-    values_seen = set()
+
+List<int> values_seen = new List<int>();
     for n in numbers:
         # If 10-n is in the values_seen set then I know that
         # I have previously seen a number that will sum with n 
         # to equal 10.  Display that pair
         if 10-n in values_seen:
-            print(n, 10-n)
+            Console.WriteLine(n, 10-n)
         # Add this number to the values_seen set 
         values_seen.add(n)
 
 display_sums([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])  
-"""
+/* 
 Should show something like (order does not matter):
 6 4
 7 3
 8 2
 9 1 
-"""
-print("===========")
+*/ 
+
+
+Console.WriteLine("===========")
 display_sums([-20, -15, -10, -5, 0, 5, 10, 15, 20]) 
 """
 Should show something like (order does not matter):
@@ -43,7 +45,7 @@ Should show something like (order does not matter):
 15 -5
 20 -10
 """
-print("===========")
+Console.WriteLine("===========")
 display_sums([5, 11, 2, -4, 6, 8, -1])
 """
 Should show something like (order does not matter):
