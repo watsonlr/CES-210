93b54206c2ca778be56d8cc076003da4d59382a3
watsonlr <watsonl@byui.edu>
2022-05-26 23:58:13 -0600

    safety

--- C-Sharp/05-teach_unique_letters-solution/05-teach_unique_letters-solution-lrw.cs
+++ C-Sharp/05-teach_unique_letters-solution/05-teach_unique_letters-solution-lrw.cs
@@ -11,52 +11,69 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 ********/
 Console.WriteLine("CSE212:  05-Teach - Problem 1 - Solution");  // Comment out this line
 
-namespace Namespace {
+string test0 = "abcdefghijklmnopqrstuvwxyZ";
+string test1 = "abcdefghijklanopqrstuvwxyz";
+string test2 = "";
+List<string> test_strings = new List<string>();
+
+
+
+test_strings.Append(test0);
+test_strings.Append(test1);
+test_strings.Append(test2);
     
-    using System.Collections.Generic;
-    
-    public static class Module {
-        
-        //  
-        //     Determine if there are any duplicate letters in the text provided.
-        //     This solution should use a set and result in O(n) performance.
-        //     
-        public static object unique_letters(object text) {
-            var found = new HashSet<object>();
-            foreach (var letter in text) {
-                // Look in set to see if letter was seen before
-                if (found.Contains(letter)) {
-                    return false;
-                }
-                // Otherwise we will add it to the set and move on
-                found.add(letter);
-            }
-            // If we made it this far then all letters were unique
-            return true;
-        }
-        
-        // An alternative solution:
-        //  
-        //     Determine if there are any duplicate letters in the text provided.
-        //     This solution should use a set and result in O(n) performance.
-        //     
-        public static object unique_letters2(object text) {
-            var unique = new HashSet<object>();
-            foreach (var letter in text) {
-                unique.add(letter);
-            }
-            // The above three lines can be simplified to just: unique = set(text)
-            // If the text has only unique letters, then the unique set should
-            // be the same size of the text string.
-            return unique.Count == text.Count;
-            // We could simplify this function to a single line:
-            // return len(set(text)) == len(text)
-        }
-        
-        public static object test1 = "abcdefghijklmnopqrstuvwxyz";
-        
-        public static object test2 = "abcdefghijklanopqrstuvwxyz";
-        
-        public static object test3 = "";
+PrintArray(test_strings);
+
+
+
+    //     Determine if there are any duplicate letters in the text provided.
+    //     This solution should use a set and result in O(n) performance.
+
+foreach (string test in test_strings)
+    {
+    Console.WriteLine("Duplicate letters in test: " + duplicate_letters(test));  // Comment out this line
     }
-}
+
+// Console.WriteLine("Duplicate letters in test1: " + duplicate_letters(test1));  // Comment out this line
+// Console.WriteLine("Duplicate letters in test2" + duplicate_letters(test2));  // Comment out this line
+// Console.WriteLine("Duplicate letters in test3" + duplicate_letters(test3));  // Comment out this line
+
+
+bool duplicate_letters(string textin)
+    {
+    bool found = false;
+    Console.WriteLine("length in is: " + textin.Length);
+    for(int i=0;i<textin.Length;i++) {
+        Console.WriteLine("doing: " + i + textin[i]);
+        found = found ^ ((textin.Substring(i+1).IndexOf(textin[i])) > i);
+        if (found) {break;}
+        }
+    return found;
+    }
+
+void PrintArray(List<string> toprint){
+    int i = 1;
+    Console.WriteLine("Got to Print Array");
+    Console.Write("[");
+    foreach (var item in toprint) {
+        Console.Write(String.Format("  {s}", item));i++;
+        if (i<=toprint.Count) {Console.Write(",");} else {Console.WriteLine(" ]");}
+        }
+    }
+
+// An alternative solution:
+//  
+//     Determine if there are any duplicate letters in the text provided.
+//     This solution should use a set and result in O(n) performance.
+    /* 
+    var unique = new HashSet<object>();
+    foreach (var letter in text) {
+        unique.add(letter);
+    }
+    // The above three lines can be simplified to just: unique = set(text)
+    // If the text has only unique letters, then the unique set should
+    // be the same size of the text string.
+    return unique.Count == text.Count;
+    // We could simplify this function to a single line:
+    // return len(set(text)) == len(text)
+    */
