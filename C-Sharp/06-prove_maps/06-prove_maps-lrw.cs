01c7f72b559664e00e1286619c010cd65012c52d
Lynn Watson <watsonl@byui.edu>
2022-05-27 22:45:23 -0600

    working on 3

--- C-Sharp/06-prove_maps/06-prove_maps-lrw.cs
+++ C-Sharp/06-prove_maps/06-prove_maps-lrw.cs
@@ -135,3 +135,51 @@ public class Translator
             return;
             }
         }
+
+
+// #############
+// # Problem 3 #
+// #############
+
+
+
+class is_anagram()
+{
+
+
+    /* 
+    Determine if 'word1' and 'word2' are anagrams.  An anagram
+    is when the same letters in a word are re-organized into a 
+    new word.  A Python dictionary is used to solve the problem.
+
+    Examples:
+    is_anagram("CAT","ACT") would return True
+    is_anagram("DOG","GOOD") would return False because GOOD has 2 O's
+
+    Important Note: When determining if two words are anagrams, you
+    should ignore any spaces.  You should also ignore cases.  For 
+    example, 'Ab' and 'Ba' should be considered anagrams
+
+    Reminder: You can access a letter by index in a Python string by 
+    using the [] notation.
+    */
+    public is_anagram
+    return true;
+
+}
+
+
+/*
+# Sample Test Cases (may not be comprehensive) 
+print("\n=========== PROBLEM 3 TESTS ===========")
+print(is_anagram("CAT","ACT")) # True
+print(is_anagram("DOG", "GOOD")) # False
+print(is_anagram("AABBCCDD", "ABCD")) # False
+print(is_anagram("ABCCD","ABBCD")) # False
+print(is_anagram("BC","AD")) # False
+print(is_anagram("Ab","Ba")) # True
+print(is_anagram("A Decimal Point", "Im a Dot in Place"))  # True
+print(is_anagram("tom marvolo riddle", "i am lord voldemort")) # True
+print(is_anagram("Eleven plus Two", "Twelve Plus One")) # True
+print(is_anagram("Eleven plus One", "Twelve Plus One")) # False
+*/
