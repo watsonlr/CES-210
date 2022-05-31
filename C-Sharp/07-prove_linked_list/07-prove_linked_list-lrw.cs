38848469df20785761db0f3505611b726831122e
watsonlr <watsonl@byui.edu>
2022-05-30 23:12:20 -0600

    testing all but reverse

--- C-Sharp/07-prove_linked_list/07-prove_linked_list-lrw.cs
+++ C-Sharp/07-prove_linked_list/07-prove_linked_list-lrw.cs
@@ -11,8 +11,23 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 ********/
 Console.WriteLine("CSE212:  07-Prove - Problems");  // Comment out this line
 
+Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
+doublyLinkedList dll = new doublyLinkedList();
 
-class doublyLinkedList
+dll.insert_tail(1);
+dll.insert_tail(1);
+dll.insert_head(2);
+dll.insert_head(2);
+dll.insert_head(2);
+dll.insert_head(3);
+dll.insert_head(4);
+dll.insert_head(5);
+dll.showList(dll.ll); // linkedlist[5, 4, 3, 2, 2, 2, 1]
+dll.insert_tail(0);
+dll.insert_tail(-1);
+dll.showList(dll.ll); // linkedlist[5, 4, 3, 2, 2, 2, 1, 0, -1]
+
+public class doublyLinkedList
     {
     /*
     Implement the LinkedList data structure.  The Node class below is an 
@@ -20,18 +35,35 @@ class doublyLinkedList
     the outer class.  To create a Node object outside of the class, we will need to 
     specify singlyLinkedList.Node
     */
-    internal class Node {  
+    public class Node {  
         public double data ;  
         public Node prev = null;  
         public Node next = null;  
         }  
 
 
-    //Initialize an empty linked list.
     Node head = null;
     Node tail = null;
 
-    void insert_head(double value)
+    public Node ll = new Node();
+
+    public void showList(Node list_to_show) {
+        Node p = new Node() ;
+        p=list_to_show;
+        Console.Write("[");
+        do {
+            Console.Write(p.data);
+            if (p.next !=null)
+                {Console.Write(", ");}
+            else {
+                Console.WriteLine("]");
+                }
+            p=p.next;
+            }
+        while(p!=null);
+        }
+
+    public void insert_head(double value)
         {
         /*
         Insert a new node at the front (i.e. the head) of the
@@ -57,7 +89,7 @@ class doublyLinkedList
     /////////////////////
     // Start Problem 1 //
     /////////////////////
-    void insert_tail(double value)
+    public void insert_tail(double value)
         {
         /*
         Insert a new node at the end (i.e. the tail) of the linked list.
@@ -66,6 +98,7 @@ class doublyLinkedList
         Node newNode = new Node();
         // If the list is empty, then point both head and tail to the new node.
         newNode.data = value;
+        Console.WriteLine("Inserting: " + value);
         newNode.next = null;    // Nobody after a new node at the tail
         if (tail == null){
             head = newNode;
@@ -83,11 +116,9 @@ class doublyLinkedList
     // Start Problem 2 //
     /////////////////////
 
-    void remove_tail()
+    public void remove_tail()
         {
-        /*
-        Remove the last node of the linked list.
-        */
+        /* Remove the last node of the linked list.  */
         if (tail == null){ // list is null, so just return
             return;
             } else {
@@ -95,11 +126,9 @@ class doublyLinkedList
             tail.next = null;
             }
         }
-    void remove_head()
+    public void remove_head()
         {
-        /*
-        Remove the first node of the linked list.
-        */
+        /* Remove the first node of the linked list.  */
         if (head == null){ // list is null, so just return
             return;
             } else {
@@ -115,18 +144,19 @@ class doublyLinkedList
     /////////////////////
     // Start Problem 3 //
     /////////////////////
-    Node find_a_node(double with_value){
+    Node find_a_node(Node search_from, double with_value){
         Node search = head;
         while (search.data != with_value) {
             search = search.next;
-        }
+            }
         if (search.data == with_value) {
-        return search; 
-        } else {
-        return null;
+            return search; 
+            } else {
+            return null;
+            }
         }
 
-    void insert_after(double newValue, double findValue) {
+    public void insert_after(double newValue, double findValue) {
         /*
         Insert 'new_value' after the first occurance of 'value' in
         the linked list.
@@ -134,46 +164,46 @@ class doublyLinkedList
         // Search for the node that matches 'value' by starting at the 
         // head of the list.
         // Create the new node
-        Node foundNode = find_a_node(findValue);
+        Node foundNode = find_a_node(head,findValue);
         /* if the 'found_node' is the tail AND the values don't match, then abort
         // just insert it at the end
         */
         if (foundNode == tail ) {
-            if (findValue != tail.data){
+            if (findValue != tail.data)
+                {
                 Console.WriteLine("Tried to insert after a value :" + findValue + ": not found");
+                }
                 else {
                 insert_tail(newValue);
                 }
-            }
-        // If wasn't the last one, so there's a node found with the value of interest
-        else {
+            } else { // If wasn't the last one, so there's a node found with the value of interest
             Node newNode = new Node();
             newNode.data = newValue;
             newNode.prev = foundNode;
             newNode.next = foundNode.next;
             foundNode.next = newNode;
             }
-        }
-    }
+            }
 
-    bool is_head(Node thisnode) { return (thisnode == head); }
-    bool is_tail(Node thisnode) { return (thisnode == tail); }
+    public bool is_head(Node thisnode) { return (thisnode == head); }
+    public bool is_tail(Node thisnode) { return (thisnode == tail); }
 
-    void remove_node(Node to_remove) {
+    public void remove_node(Node to_remove) {
         if (is_head(to_remove)) {remove_head();return;}
         if (is_tail(to_remove)) {remove_tail();return;}
         // Just someone in the middle
         to_remove.prev.next = to_remove.next; //change link previous
         to_remove.next.prev = to_remove.prev; //change link following
-    }
+        }
 
-
-    void remove_find(double findValue) {
-        Node foundNode = find_a_node(findValue);
+    public void remove_find(double findValue) {
+        Node foundNode = find_a_node(head,findValue);
         if (foundNode == null) {
             Console.WriteLine("Tried to delete a node with unfound value :" + findValue + ": not found");
+            }
         else {
             remove_node(foundNode);
+            }
         }
 
     /////////////////////
@@ -186,80 +216,57 @@ class doublyLinkedList
     /////////////////////
     // Start Problem 4 //
     /////////////////////
-    def replace(self, old_value, new_value):
-        /*
-        Searrch for all instances of 'old_value' and replace the value 
-        to 'new_value'.
-        */
-        pass
-
-    //
+    /* Search for all instances of 'old_value' and replace the value to 'new_value'.  */
+    public void replace(double old_value, double new_value) {
+        Node found = find_a_node(head,old_value);
+        while ((found != tail) && (found.data == old_value )) {
+            found.data = new_value;
+            found = find_a_node(found.next,old_value);  // start search from the next location
+            }
+        }
+    /////////////////////
     // End Problem 4 //
-    //
+    /////////////////////
 
-    def __iter__(self):
-        /*
-        Iterate foward through the Linked List
-        */
-        curr = self.head  // Start at the begining since this is a forward iteration.
-        while curr is not None:
-            yield curr.data  // Provide (yield) each item to the user
-            curr = curr.next // Go forward in the linked list
-
-    //
+    /////////////////////
     // Start Problem 5 //
-    //
-    def __reversed__(self):
-        /*
-        Iterate backward through the Linked List
-        */
-        yield "???"  // Replace this when you implement your solution
+    /////////////////////
+    Node reversed_ll(Node tail_to_become_head) {
+        /* Algorithm for reversing the list:
+            Is the list empty or one element? If yes, then it is already reversed.
+            Otherwise, create a new empty list.
 
-    //
+            In a loop, remove the first item from the old list and add it to the start of the new list.
+            Loop until the first list is empty.
+        */
+        Node reversed_ll = new Node(); //start a new list
+        reversed_ll = tail_to_become_head;
+        reversed_ll.data = tail_to_become_head.data;
+        reversed_ll.next = tail_to_become_head.prev;
+        reversed_ll.prev = null;
+        while (tail_to_become_head.prev != null) {
+            Node addReversed = new Node(); //start a new list
+            addReversed.next = tail_to_become_head.prev;
+            addReversed.data = tail_to_become_head.data;
+            }
+        return reversed_ll;
+        }
+    }
+        
+
+    /////////////////////
     // End Problem 5 //
-    //
-
-    def __str__(self):
-        /*
-        Return a string representation of the linked list.
-        */
-        output = "linkedlist["
-        first = True
-        for value in self:
-            if first:
-                first = False
-            else:
-                output += ", "
-            output += str(value)
-        output += "]"
-        return output
-
-}
-
-class testLinkedList {
+    /////////////////////
 
 // Sample Test Cases (may not be comprehensive) 
-print("\n=========== PROBLEM 1 TESTS ===========")
-ll = LinkedList()
-ll.insert_tail(1)
-ll.insert_head(2)
-ll.insert_head(2)
-ll.insert_head(2)
-ll.insert_head(3)
-ll.insert_head(4)
-ll.insert_head(5)
-print(ll) // linkedlist[5, 4, 3, 2, 2, 2, 1]
-ll.insert_tail(0)
-ll.insert_tail(-1)
-print(ll) // linkedlist[5, 4, 3, 2, 2, 2, 1, 0, -1]
-
-print("\n=========== PROBLEM 2 TESTS ===========")
+/* 
+Console.WriteLine("\n=========== PROBLEM 2 TESTS ===========")
 ll.remove_tail()
 print(ll) // linkedlist[5, 4, 3, 2, 2, 2, 1, 0]
 ll.remove_tail()
 print(ll) // linkedlist[5, 4, 3, 2, 2, 2, 1]
 
-print("\n=========== PROBLEM 3 TESTS ===========")
+Console.WriteLine("\n=========== PROBLEM 3 TESTS ===========")
 ll.insert_after(3, 3.5)
 ll.insert_after(5, 6)
 print(ll) // linkedlist[5, 6, 4, 3, 3.5, 2, 2, 2, 1]
@@ -278,7 +285,7 @@ print(ll) // linkedlist[4, 3.5, 2, 2, 2]
 ll.remove(2)
 print(ll) // linkedlist[4, 3.5, 2, 2]
 
-print("\n=========== PROBLEM 4 TESTS ===========")
+Console.WriteLine("\n=========== PROBLEM 4 TESTS ===========")
 ll.replace(2, 10)
 print(ll) // linkedlist[4, 3.5, 10, 10]
 ll.replace(7, 5)
@@ -287,7 +294,6 @@ ll.replace(4, 100)
 print(ll) // linkedlist[100, 3.5, 10, 10]
 
 
-print("\n=========== PROBLEM 5 TESTS ===========")
+Console.WriteLine("\n=========== PROBLEM 5 TESTS ===========")
 print(list(reversed(ll)))  //// [10, 10, 3.5, 100]
-
-}    
+*/
