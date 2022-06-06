dc9c547e7e9c860cb32c342a9df04db603c554b5
Lynn Watson <watsonl@byui.edu>
2022-06-06 07:09:29 -0600

    Initial, with testing method

--- C-Sharp/05-prove_set_operations/05-prove_set_operations.cs
+++ C-Sharp/05-prove_set_operations/05-prove_set_operations.cs
@@ -10,3 +10,57 @@ GitHub repository, unshared Google Drive folder) is acceptable.
 //--- Created from: dotnet new console. See https://aka.ms/new-console-template for more information ---//
 ********/
 Console.WriteLine("CSE212:  05-Prove - Problem 1");  // Comment out this line
+
+
+class set_operations{
+    public int[] intersection (int[] set1, int[] set2) {
+        /*
+        Perform an intersection between 2 sets.  An intersection will contain
+        the items in common between both sets.  Do not use the set 
+        operators (+, -, *, &, |) and functions (intersection, union) 
+        that are built-in to Python.
+        */
+        int[] answer = {};
+        return answer;
+        }
+    public int[] union (int[] set1, int[] set2) {
+        /*
+        Perform a union between 2 sets.  A union will contain all the items
+        from both sets.   Do not use the set operators (+, -, *, &, |)
+        and functions (intersection, union) that are built-in to Python.
+        */
+        int[] answer = {};
+        return answer;
+    }
+
+
+
+    public bool test(int[] testing_in, int[] answer_in) {
+        // To make sure they are sets, I'll use the built-in method, convert them both, and compare
+        HashSet<int> t = testing_in.ToHashSet();
+        HashSet<int> a = answer_in.ToHashSet();
+        return (t==a);
+        }
+    public void do_all_tests(){
+        // Just goes thru a couple of test cases, checks to see that they pass and prints the status
+        bool passed = true;
+        int[] s1 = {1,2,3,4,5}; int[] s2 = {4,5,6,7,8};
+        int[] intersect1_ans = {4,5}; int[] union1_ans = {1, 2, 3, 4, 5, 6, 7, 8};
+        // Test 1
+        passed &= test(intersection(s1,s2),intersect1_ans);
+        passed &= test(union(s1,s2),union1_ans);
+        // Test 2
+        int[] t1 = {1,2,3,4,5}; int[] t2 = {6,7,8,9,10};
+        int[] intersect2_ans = {}; int[] union2_ans = {1, 2, 3, 4, 5, 6, 7, 8,9,10};
+        passed &= test(intersection(t1,t2),intersect2_ans);
+        passed &= test(union(t1,t2),union2_ans);
+
+        if (passed) {
+            Console.WriteLine("Congratulations!  Tests Passed");
+            else {
+            Console.WriteLine("TESTS Failed");
+            }
+        }
+        }
+
+    }
